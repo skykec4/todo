4cd98647da1eb12096e665a13445e53a08d3b963
unknown <skykec4@naver.com>
2018-01-11 17:56:03 +0900

    Styleing the To Do Component

--- Todo.js
+++ Todo.js
@@ -14,18 +14,47 @@ export default class Todo extends Component {
     isCompleted: false
   };
   render() {
-    const { isCompleted } = this.state;
+    const { isCompleted, isEditing } = this.state;
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this._toglleComplete}>
-          <View
+        <View style={styles.column}>
+          <TouchableOpacity onPress={this._toglleComplete}>
+            <View
+              style={[
+                styles.circle,
+                isCompleted ? styles.completedCircle : styles.uncompletedCircle
+              ]}
+            />
+          </TouchableOpacity>
+          <Text
             style={[
-              styles.circle,
-              isCompleted ? styles.completedCircle : styles.uncompletedCircle
+              styles.text,
+              isCompleted ? styles.completedText : styles.uncompletedText
             ]}
-          />
-        </TouchableOpacity>
-        <Text style={styles.text}>Hello Text</Text>
+          >
+            Hello Text
+          </Text>
+        </View>
+        {isEditing
+          ? <View style={styles.actions}>
+              <TouchableOpacity onPressOut={this._finishEditing}>
+                <View style={styles.actionContainer}>
+                  <Text style={styles.actionText}>○</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          : <View style={styles.actions}>
+              <TouchableOpacity onPressOut={this._startEditing}>
+                <View style={styles.actionContainer}>
+                  <Text style={styles.actionText}>수정</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <View style={styles.actionContainer}>
+                  <Text style={styles.actionText}>X</Text>
+                </View>
+              </TouchableOpacity>
+            </View>}``
       </View>
     );
   }
@@ -36,6 +65,16 @@ export default class Todo extends Component {
       };
     });
   };
+  _startEditing = () => {
+    this.setState({
+      isEditing: true
+    });
+  };
+  _finishEditing = () => {
+    this.setState({
+      isEditing: false
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -44,7 +83,8 @@ const styles = StyleSheet.create({
     borderBottomColor: "#bbb",
     borderBottomWidth: StyleSheet.hairlineWidth,
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    justifyContent: "space-between"
   },
   circle: {
     width: 30,
@@ -63,6 +103,27 @@ const styles = StyleSheet.create({
     borderColor: "#bbb"
   },
   uncompletedCircle: {
-    borderColor: "#F23657"
+    borderColor: "#008000"
+  },
+  completedText: {
+    color: "#bbb",
+    textDecorationLine: "line-through"
+  },
+  uncompletedText: {
+    color: "#353839"
+  },
+  column: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: width / 2,
+    justifyContent: "space-between"
+  },
+  actions: {
+    flexDirection: "row"
+  },
+  actionContainer: {
+    /*margin: 10*/
+    marginVertical: 10,
+    marginHorizontal: 10
   }
 });
